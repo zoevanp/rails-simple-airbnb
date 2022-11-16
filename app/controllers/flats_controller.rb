9ed6beec8b02d9eedf9f0f0c7314cd4479dea9ed
Zoe vanPerlstein <zoe.vanperlstein1@gmail.com>
2022-11-16 17:19:59 +0000

    rails flats back-end only

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,49 @@
+class FlatsController < ApplicationController
+  before_action :set_flat, only: %i[show edit update destroy]
+
+  def index
+    @flats = Flat.all
+  end
+
+  def show
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to @flat, notice: 'Congratulations on your new flat!'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @flat.update(flat_params)
+      redirect_to flat_path(@flat), notice: 'flat was successfully updated'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @flat.destroy
+    redirect_to flats_path, notice: 'Flat was successfully destroyed.'
+  end
+
+  private
+
+  def set_flat
+    @flat = Flat.find(params[:id])
+  end
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'La maison des van P',
+  address: 'place communale de Lasne',
+  description: 'ancienne ferme rénovée, avec une cour intérieure et un jardin. Animaux bienvenus.',
+  price_per_night: 250,
+  number_of_guests: 7
+)
+
+Flat.create!(
+  name: 'Les grottes',
+  address: 'Sivergues, Vaucluse',
+  description: 'jolie maison en pierre perdue dans les montagnes du Vaucluse. Arbres truffiers.',
+  price_per_night: 1800,
+  number_of_guests: 20
+)
+
+Flat.create!(
+  name: 'Quinta dos Ribeiros',
+  address: 'Monte Filipe, Alpalhao',
+  description: 'Le plus bel endroit du monde. Jolie ferme avec des ânes.',
+  price_per_night: 500,
+  number_of_guests: 25
+)
